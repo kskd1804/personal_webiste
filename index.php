7e9c7921a05676033fdcfbf965ca9617348d23d0
Kalvakolanu Sai Kousthubha Das <kskd1804@gmail.com>
2017-12-06 20:21:09 +0530

    Basic design

---
+++ index.php
@@ -0,0 +1,155 @@
+<html>
+<head>
+	<title>Sai Kousthubha Das Kalvakolanu</title>
+	<link rel='stylesheet' type='text/css' href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"/>
+	<link rel='stylesheet' type='text/css' href="css/styles.css"/>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	<link href="https://fonts.googleapis.com/css?family=Josefin+Sans" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+</head>
+<body>
+	<nav class="navbar navbar-default">
+		  <div class="container-fluid">
+		    <div class="navbar-header">
+		      <a class="navbar-brand" href="http://www.saikousthubhadask.tk/">Sai Kousthubha Das Kalvakolanu</a>
+		    </div>
+		    <div class="collapse navbar-collapse navbar-ex1-collapse">
+		    <ul class="nav navbar-nav" style="margin-left: 50px;">
+		      <li><a href="index.php">About</a></li>
+		      <li><a href="index.php#projects">Projects</a></li>
+		      <li><a href="index.php#research">Research Work</a></li>
+		      <li><a href="index.php#contact">Contact</a></li>
+		    </ul>
+			</div>
+		    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+			    <span class="sr-only">Toggle navigation</span>
+			    <span class="icon-bar"></span>
+			    <span class="icon-bar"></span>
+			    <span class="icon-bar"></span>
+			  </button>
+		  </div>
+		</nav>
+	<div class="content">
+		<div style="padding:10px;">
+			<h1>#aboutMe</h1>
+			<p style="float: left;margin-right: 30px;width: 200px">
+				<img src="Images/me.jpg" width="200" style="border: 10px solid #e6e6e6;box-shadow: 1px 1px 1px #CCC" />
+			</p>
+			<p style="float: left;height: 150px;width:600px;padding-top: 10px;text-align: justify">I am an autodidact exploring the tantalizing areas of computer science with the desire to build and maintain artificially intelligent systems. <br>I am the Founder & CEO of Coders Domicile, a company which provides technical support, consultancy and business solutions.<br>I currently serve as the Joint Secretary of IEEE MVSR Student Branch, one of the most active student branches of IEEE Hyderabad Section. We at IEEE MVSR conduct technical talks, workshops and a few social events, which help the student and the society.<br><br>My interests include: Machine Learning, Natural Language Processing, Robot Systems, Data Mining & Computer Vision
+			</p>
+			<div style="clear: both;"/>
+		</div>
+		<h1 style="margin-top: 35px;margin-bottom: 5px;">#positionsOfResponsibility</h1>
+		<ul>
+			<li><strong>IEEE MVSR Student Branch</strong>, <i>Joint Secretary</i><br>August 2017</li>
+			<li><strong>Deloitte Engineering School Maverick</strong>, <i>Social Media Strategist</i><br>February 2017</li>
+			<li><strong>Information Technology Student Association</strong>, MVSR Engineering College, <i>Cultural Secretary</i><br>February 2016</li>
+			<li><strong>Coders Domicile</strong>, <i>Founder & CEO</i><br>September 2015</li>
+		</ul>
+		<h1 style="margin-top: 35px;margin-bottom: 5p<li><strong>IEEE MVSR Student Branch</strong>, <i>Joint Secretary</i><br>August 2017</li>x;">#accomplishments</h1>
+		<ul>
+			<li><strong>Machine Learning</strong>, Stanford University on Coursera<br>September 2017 | <a target="_blank" href="https://www.coursera.org/account/accomplishments/verify/SWDAHHWP93YV">View Certificate</a></li>
+			<li><strong>Ethical Hacking</strong>, TechDefense<br>January 2017</li>
+		</ul>
+		<h1 style="margin-top: 35px;margin-bottom: 5px;" id="projects">#projectsAndInternships</h1>
+		<ul>
+			<li>
+				<h3><strong>Object Recognition & Tracking System</strong>, <i>MVSR Engineering College</i> <font size="2">(to be completed)</font><br></h3>
+				<strong>Description:</strong>
+				<p style="text-align: justify;">This project deals with recognize an object of interest from an image or a video stream and track its motion. Recognition of the object of interest is done using a <a href="https://docs.opencv.org/3.3.0/d7/d8b/tutorial_py_face_detection.html">Haar Cascade and Open CV</a>. As part of the project, we intend to develop a Haar Cascade for automobile detection using which our application will be able to recognize automobiles present in the input image or video</p>
+				<strong>Duration of Project: </strong> August 2017 - Present<br>
+				<strong>Contributors:</strong> Sai Kousthubha Das Kalvakolanu*, Archith Reddy Muddam, Sai Charan Shivarathri <font size="2">(* indicates lead)</font>
+			</li>
+			<li>
+				<h3><strong>Organizational Knowledge Base</strong>, <i>NRSC, Indian Space Research Organization (ISRO)</i> <font size="2">(Internship)</font><br></h3>
+				<strong>Description:</strong>
+				<p style="text-align: justify;">The organizational knowledge base is expected to store the facts, beliefs and lessons that the officials have learned from the various missions of ISRO. Department-wise access controls are built into the application being developed. The application provides an easy to use interface using which the data in the knowledge base can be accessed.
+				</p>
+				<strong>Duration of Project: </strong> October 2017 - November 2017<br>
+				<strong>Contributors:</strong> Sai Kousthubha Das Kalvakolanu*, Archith Reddy Muddam, Pranav Surampudi <font size="2">(* indicates lead)</font><br>
+				<strong>Links:</strong> <a href="docs/NRSC_Certificate.pdf">View Certificate</a>
+			</li>
+			<li>
+				<h3><strong>Kraken 1.0</strong>, A Linux Distribution<br></h3>
+				<strong>Description:</strong>
+				<p style="text-align: justify;">Kraken 1.0 is an operating system desinged to work on low-end systems. It is an LFS system developed using the Slackware Linux, with all the necessary package pre-installed. Very soon, I plan to develop an improved version of Kraken 1.0 with a few additional features called Kraken 2.0.</p>
+				<strong>Duration of Project: </strong> January 2017 - May 2017<br>
+				<strong>Contributors:</strong> Sai Kousthubha Das Kalvakolanu
+			</li>
+			<li>
+				<h3><strong>Air & Sound Pollution Monitoring System</strong>, <i>MVSR Engineering College</i><br></h3>
+				<strong>Description:</strong>
+				<p style="text-align: justify;">This project was started in order to gain control over the gradually increasing pollution levels on earth. The project is developed using an Arduino Mega, FC-04 Sound Sensor and MQ-135 Gas Sensor. The levels of pollution in its surroundings are constantly monitored and logged in a database which is connected to the Arduino.</p>
+				<strong>Duration of Project: </strong> February 2017 - April 2017<br>
+				<strong>Contributors:</strong> Sai Kousthubha Das Kalvakolanu*, Archith Reddy Muddam, Ravi Kiran Panchumarthi <font size="2">(* indicates lead)</font>
+			</li>
+			<li>
+				<h3><strong>Home Automation System</strong> (Yantra)<br></h3>
+				<strong>Description:</strong>
+				<p style="text-align: justify;">Yantra was designed with the vision to make every home come to life for a very low price. The project is split into two parts which are an Arduino Board and a database. The arduino is connected to the database, which stores all the instructions that were requested by the user and also the logs of the board. The system (arduino and the database together) can be controlled using a graphical user interace which is available to the authorized personnel.</p>
+				<strong>Duration of Project: </strong> August 2016 - October 2016<br>
+				<strong>Contributors:</strong> Sai Kousthubha Das Kalvakolanu
+			</li>
+			<li>
+				<h3><strong>Member Management System</strong>, <i>K C Das Trust.</i><br></h3>
+				<strong>Description:</strong>
+				<p style="text-align: justify;">For an organization with an massive number of members across the globe, management of members and communication between them becomes an tedious task. To address this issue, we built Member Management System, an application which aids in maintaining records of events organized, number of participants and their contribution to each event.</p>
+				<strong>Duration of Project: </strong> February 2016 - April 2016<br>
+				<strong>Contributors:</strong> Coders Domicile
+			</li>
+			<li>
+				<h3><strong>Employee Management System</strong>, <i>Kalva Engineers PVT. LTD.</i><br></h3>
+				<strong>Description:</strong>
+				<p style="text-align: justify;">Employee Management System is an application developed to automate the registry of operations occuring in an organization. It enables the organization to maintain accurate records of various activities being performed on a daily basis and hence eliminates the need for the directors of the organization to be physically present to monitor the work. The project also includes modules which aid in generating reports and graphs for each month to analyze the growth of the company.</p>
+				<strong>Duration of Project: </strong> September 2015 - December 2015<br>
+				<strong>Contributors:</strong> Coders Domicile
+			</li>
+			<li>
+				<h3><strong>Websites</strong><br></h3>
+				<ul>
+					<li style="margin-bottom: 2px">Nirvana Technologies PTE LTD.<br>February 2016 | <a href="http://nirvanatechnologies.biz">View Website</a></li>
+					<li style="margin-bottom: 2px">Kalva Engineers PVT. LTD.<br>July 2015 | <a href="http://kalva.net/">View Website</a></li>
+					<li style="margin-bottom: 2px">K C Das Trust<br>January 2014 | <a href="http://kcdastrust.org">View Website</a></li>
+				</ul>
+			</li>
+		</ul>
+		<h1 style="margin-top: 35px;margin-bottom: 5px;" id="research">#researchWorks</h1>
+		<ul>
+			<li>
+				<strong>Offline Signature Verification using ANN</strong>, <i>MVSR Engineering College</i><br>
+				<strong>Description:</strong>
+				<p style="text-align: justify;">Currently working to develop an algorithm capable of detecting a signature in an input image, cropping the signature and finally verify the authenticity of the signature.</p>
+				<strong>Duration: </strong> September 2017 - Present<br>
+				<strong>Contributor(s):</strong> Dr. V. Hanumandlu (Prof.), Sai Kousthubha Das Kalvakolanu (Student), Haripriya Dharmala(Student), Archith Reddy Muddam (Student), Srihitha Trovagunta (Student).
+			</li>
+			<li>
+				<strong>Discovery of Number of Clusters for an Unsupervised Learning</strong>, <i>MVSR Engineering College</i><br>
+				<strong>Abstract:</strong>
+				<p style="text-align: justify;">Though clusters can be found using K-Means Clustering algorithm, the algorithm requires the input data set and the number of clusters in the data set. In this paper, we study the behavior of K-Means algorithm with respect to number and choice of the initial cluster centers. We present an algorithm to discover the number of clusters in the given data. We apply this algorithm along with K-Means to demonstrate a completely unsupervised learning.</p>
+				<strong>Duration: </strong> March 2017 - November 2017<br>
+				<strong>Contributor(s):</strong> Sai Kousthubha Das Kalvakolanu (Student)*, V. Ashwini Kumar (Assoc. Prof.)*, Archith Reddy Muddam (Student), Meghana Sreepada (Student) <font size="2">(* indicates primary)</font><br>
+				<strong>Links: </strong><a href="docs/Springer.pdf">[.pdf]</a>
+			</li>
+		</ul>
+		<h1 style="margin-top: 35px;margin-bottom: 5px;" id="contact">#address</h1>
+		<p>
+			Hno: 11-13-147,<br>Street: 3, Alkapuri Colony,<br>Ramakrishnapuram, <br>Hyderabad - 500035<br>India
+		</p>
+		<h1 style="margin-top: 35px;margin-bottom: 5px;">#contactInfo</h1>
+		<ul>
+			<li><strong>mobile:</strong> +91 7675038333</li>
+			<li><strong>email:</strong> <a href="mailto:kskd1804@gmail.com">kskd1804@gmail.com</a></li>
+		</ul>
+		<h1 style="margin-top: 35px;margin-bottom: 5px;">#otherLinks</h1>
+		<ul>
+			<li><strong>LinkedIn:</strong> <a href="https://www.linkedin.com/in/sai-kousthubha-das-kalvakolanu-224761136/">linkedin.com/in/sai-kousthubha-das-kalvakolanu-224761136/</a></li>
+			<li><strong>GitHub:</strong> <a href="https://www.github.com/kskd1804">kskd1804</a></li>
+		</ul>
+		</div>
+	</div>
+	<footer>
+		&copy; <?php echo date('Y');?> - Sai Kousthubha Das Kalvakolanu
+		<a href="https://www.facebook.com/kskd.97"><div style="float: right;font-size: 12px;"><img src="https://www.seeklogo.net/wp-content/uploads/2016/09/facebook-icon-preview-200x200.png" width="25">Find Me @ Facebook</div></a>
+	</footer>
+</body>
+</html>
